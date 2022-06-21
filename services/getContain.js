b277e78c8e9a53ddac7fd473aea851af889ef239
error404LikeAnArson <93117779+error404LikeAnArson@users.noreply.github.com>
2022-06-21 13:51:25 +0200

    Add files via upload

--- services/getContain.js
+++ services/getContain.js
@@ -28,35 +28,35 @@ function getContact(){
 
 
 //get about text before resolve of problem "connection before page display"
-function getText(){
-    return {
-        titlePage: "AJT services, votre agence de nettoyage 100% orientée vers les professionnels!",
-        text1: "Nous nous chargeons pour vous, efficacement du nettoyage et de la desinfection de vos établissements.", 
-        text2CatchPhrase: "Notre mission?",
-        text2: "Faire briller les moindre recoins de vos entreprises! nous intervenons principalement, dans des locations de vacances, les parties commune de residences, les bureaux, restaurants snacks, et autres commerces.",
-        text3: "Spécialisée dans le nettoyage de locaux professionnels, AJT Services est aujourd’hui connue pour sa fiabilité et son amour du travail bien réalisé." 
-    };
-}
-
-// read text about in database
-// async function getTextPre(){
-//     const contain = await Contain.findOne( {id: 1} );
-//     const text = await contain.aboutText.id(ObjectId("62ab2cb6f2867b33bc19df78"));
+// function getText(){
 //     return {
-//         titlePage: text.titlePage,
-//         text1: text.text1, 
-//         text2CatchPhrase: text.text2CatchPhrase,
-//         text2: text.text2,
-//         text3: text.text3 
+//         titlePage: "AJT services, votre agence de nettoyage 100% orientée vers les professionnels!",
+//         text1: "Nous nous chargeons pour vous, efficacement du nettoyage et de la desinfection de vos établissements.", 
+//         text2CatchPhrase: "Notre mission?",
+//         text2: "Faire briller les moindre recoins de vos entreprises! nous intervenons principalement, dans des locations de vacances, les parties commune de residences, les bureaux, restaurants snacks, et autres commerces.",
+//         text3: "Spécialisée dans le nettoyage de locaux professionnels, AJT Services est aujourd’hui connue pour sa fiabilité et son amour du travail bien réalisé." 
 //     };
 // }
 
-// function getText(){
-//     getTextPre().then(aboutText => {
-//         return aboutText;
-//     })
-//         .catch(error => {});
-// }
+// read text about in database
+async function getTextPre(){
+    const contain = await Contain.findOne( {id: 1} );
+    const text = await contain.aboutText.id(ObjectId("62ab2cb6f2867b33bc19df78"));
+    return {
+        titlePage: text.titlePage,
+        text1: text.text1, 
+        text2CatchPhrase: text.text2CatchPhrase,
+        text2: text.text2,
+        text3: text.text3 
+    };
+}
+
+function getText(){
+    getTextPre().then(aboutText => {
+        return aboutText;
+    })
+        .catch(error => {});
+}
 
 
 
